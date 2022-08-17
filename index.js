870036ac6425e967f5e0903eb07302b849a512e8
edwin <ngisahedwin@gmail.com>
2022-08-17 16:06:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+
+    return `This Saturday, I want to ${activity}!`;
+
+}
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+const mondayWork = function(activity = "go to the office"){
+
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(flair= "*"){
+
+    return function(adjective = "special"){
+
+        return `You are ${flair}${adjective}${flair}!`
+
+    }
+}
+console.log(wrapAdjective()());
+console.log(wrapAdjective("||")("a dedicated programmer"));
